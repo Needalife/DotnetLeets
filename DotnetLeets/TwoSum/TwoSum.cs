b76547de11bc3d5af75b94eb9b7e38ef0bceadaf
Nguyen Duc Anh <gauakanguyen@gmail.com>
2026-05-08 14:16:46 +0700

    Create TwoSum.cs

---
+++ DotnetLeets/TwoSum/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿namespace DotnetLeets.TwoSum
+{
+    internal class TwoSum
+    {
+        public static int[]? ReturnIndices(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+
+                if (dict.ContainsKey(complement))
+                {
+                    return [i, dict[complement]];
+                }
+
+                dict[nums[i]] = i;
+            }
+
+            return null;
+        }
+    }
+}
