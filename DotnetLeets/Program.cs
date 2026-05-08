96f8fcef354546e1fd3472cd6423a910e10a37b0
Nguyen Duc Anh <gauakanguyen@gmail.com>
2026-05-08 14:16:55 +0700

    ValidParentheses

---
+++ DotnetLeets/Program.cs
@@ -0,0 +1,14 @@
+﻿using DotnetLeets.ValidParentheses;
+
+class Program
+{
+    static void Main()
+    {
+        string s = "[]))";
+
+        var result = ValidParentheses.IsValid(s);
+
+        Console.WriteLine($"Input: {s}");
+        Console.WriteLine($"Output: {result}");
+    }
+}
---
+++ DotnetLeets/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,54 @@
+﻿namespace DotnetLeets.ValidParentheses
+{
+    internal class ValidParentheses
+    {
+        public static bool IsValid(string s)
+        {
+            if  (s.Length %2 != 0) {
+                Console.WriteLine($"Bracket is odd");
+                return false;
+            }
+
+            var stack = new Stack<char>();
+            var closingBracket = new Dictionary<char, char>()
+            {
+                { ')', '(' },
+                { ']', '[' },
+                { '}', '{' }
+            };
+            var openingBracket = new HashSet<char>()
+            {
+                { '(' },
+                { '[' },
+                { '{' }
+            };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (openingBracket.Contains(s[i]))
+                {
+                    stack.Push(s[i]);
+                    continue;
+                }
+
+                if (closingBracket.ContainsKey(s[i]) && stack.Count > 0)
+                {
+                    if (stack.Peek() == closingBracket[s[i]])
+                    {
+                        stack.Pop();
+                    } else
+                    {
+                        Console.WriteLine($"Bracket {s[i]} does not match with {stack.Peek()}");
+                        return false;
+                    } 
+                } else
+                {
+                    Console.WriteLine($"No opening/closing bracket found for {s[i]} at iteration {i}");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
