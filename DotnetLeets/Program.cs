49f6ecb197e826f4a73aa1939983cf900cd9b17b
Nguyen Duc Anh <gauakanguyen@gmail.com>
2026-05-12 09:12:22 +0700

    redo project structure

--- DotnetLeets/Program.cs
+++ DotnetLeets/Program.cs
@@ -1,19 +1,42 @@
-﻿using DotnetLeets.GroupAnagrams;
+﻿using DotnetLeets.Core;
+using DotnetLeets.Problems.Arrays;
 
 class Program
 {
     static void Main()
     {
-        string [] s = ["eat", "tea", "tan", "ate", "nat", "bat"];
+        List<ILeetProblem> problems =
+        [
+            new TwoSum()
+        ];
 
-        var result = GroupAnagrams.ReturnGroupsOfString(s);
+        Console.WriteLine("==== DOTNET LEETS ====\n");
 
-        Console.WriteLine($"Input: [{string.Join(", ", s)}]");
-
-        Console.WriteLine($"Groupings:");
-        foreach (var item in result)
+        for (int i = 0; i < problems.Count; i++)
         {
-            Console.WriteLine($"[{string.Join(", ", item)}]");
+            Console.WriteLine($"{i + 1}. {problems[i].Name}");
         }
+
+        Console.Write("\nSelect problem: ");
+
+        string? input = Console.ReadLine();
+
+        bool valid = int.TryParse(input, out int choice);
+
+        if (!valid)
+        {
+            Console.WriteLine("Invalid input.");
+            return;
+        }
+
+        if (choice < 1 || choice > problems.Count)
+        {
+            Console.WriteLine("Problem not found.");
+            return;
+        }
+
+        Console.Clear();
+
+        problems[choice - 1].Run();
     }
 }
