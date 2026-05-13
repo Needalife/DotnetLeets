bff58283cceacf34220aba4d2ed3a70e0a442734
Nguyen Duc Anh <gauakanguyen@gmail.com>
2026-05-13 09:08:12 +0700

    Create RomanToInteger.cs

---
+++ DotnetLeets/Problems/Easy/RomanToInteger.cs
@@ -0,0 +1,71 @@
+﻿using DotnetLeets.Core;
+
+namespace DotnetLeets.Problems.Easy
+{
+    internal class RomanToInteger: ILeetProblem
+    {
+        public string Name => "Roman to Integer";
+        public List<string> Tag => ["Math", "String"];
+
+        public void Run()
+        {
+            Console.WriteLine($"=== {Name} ===\n");
+            var tests = new Dictionary<string, int>
+            {
+                { "III", 3 },
+                { "IV", 4 },
+                { "VI", 6},
+                { "MCMXCIV", 1994 },
+                { "V", 5 },
+                { "MMMDCCCLXXXVIII", 3888 },
+                { "MDCLXVI", 1666 },
+                { "MCDLXXVI", 1476 },
+                { "MCDLXXIV", 1474 },
+                { "MMCDXXV", 2425 }
+            };
+
+            foreach ( var test in tests ) 
+            {
+                Test(test.Key, test.Value);
+            }
+        }
+
+        private static void Test(string s, int expected)
+        {
+            Console.WriteLine($"Input: {s}");
+            var output = Solve(s);
+            Console.WriteLine($"\nExpected: {expected}");
+            Console.WriteLine($"Output: {output}\n");
+            Console.WriteLine(output == expected ? "pass" : "fail");
+            Console.WriteLine("\n-------------------\n");
+        }
+
+        private static int Solve(string s)
+        {
+            var dict = new Dictionary<char, int>
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            var num = dict[s[0]];
+            for (int i = 1; i < s.Length; i ++)
+            {
+                if (dict[s[i]] <= dict[s[i - 1]])
+                {
+                   num += dict[s[i]];
+                } else
+                {
+                    num += dict[s[i]] - dict[s[i - 1]] * 2;
+                } 
+            }
+
+            return num;
+        }
+    }
+}
