8f54e81574ab3fb42b182b39e3418a507e0995f4
Vally <113294564+Needalife@users.noreply.github.com>
2026-05-18 15:08:14 +0700

    Create MergeTwoSortedList.cs

---
+++ DotnetLeets/Problems/Easy/MergeTwoSortedList.cs
@@ -0,0 +1,100 @@
+﻿using DotnetLeets.Core;
+using static DotnetLeets.Core.LinkedListHelper;
+
+namespace DotnetLeets.Problems.Easy
+{
+    internal class MergeTwoSortedList : ILeetProblem
+    {
+        public string Name => "Merge Two Sorted List";
+        public List<string> Tag => ["Linked List", "Recursion"];
+        public void Run()
+        {
+            Console.WriteLine($"=== {Name} ===\n");
+
+            var testCases = new List<((Node?, Node?) input, Node? expected)>
+            {
+                (
+                    (Create(1,2,4), Create(1,3,4)),
+                    Create(1,1,2,3,4,4)
+                ),
+                (
+                    (Create(), Create()),
+                    Create()
+                ),
+                (
+                    (Create(), Create(0)),
+                    Create(0)
+                ),
+                (
+                    (Create(5), Create(1,2,3)),
+                    Create(1,2,3,5)
+                ),
+                (
+                    (Create(1,2,3), Create()),
+                    Create(1,2,3)
+                ),
+                (
+                    (Create(2,4,6), Create(1,3,5)),
+                    Create(1,2,3,4,5,6)
+                ),
+                (
+                    (Create(1,1,1), Create(1,1)),
+                    Create(1,1,1,1,1)
+                )
+            };
+
+            TestHelper.TestAllCases(
+                testCases,
+                Solve,
+                ComparedLinkedList
+            );
+        }
+
+        private static Node Solve(Node list1, Node list2)
+        {
+            var dummy = new Node();
+            var current = dummy;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                } else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+
+                current = current.next;
+            }
+
+            if (list1 != null)
+            {
+                current.next = list1;
+            }
+
+            if (list2 != null)
+            {
+                current.next = list2;
+            }
+
+            return dummy.next;
+        }
+
+        public static bool ComparedLinkedList(Node? a, Node? b)
+        {
+            while (a != null && b != null)
+            {
+                if (a.val != b.val)
+                {
+                    return false;
+                }
+                a = a.next;
+                b = b.next;
+            }
+            return a == null && b == null;
+        }
+    }
+}
