673176401f02545f4c56f030e92e55038494e725
Vally <113294564+Needalife@users.noreply.github.com>
2026-05-21 16:21:00 +0700

    tbc

---
+++ DotnetLeets/Problems/Hard/MergeKSortedList.cs
@@ -0,0 +1,16 @@
+﻿
+using DotnetLeets.Core;
+
+namespace DotnetLeets.Problems.Hard
+{
+    internal class MergeKSortedList : ILeetProblem
+    {
+        public string Name => "Merge K Sorted List";
+        public List<string> Tag => ["Linked List", "Divide and Conquer", "Heap"];
+        public void Run()
+        {
+            Console.WriteLine($"=== {Name} ===\n");
+        }
+
+    }
+}
